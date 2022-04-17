94d96b3ae5442c5c7c48b769af8d5270c647d199
jewelCode <jewelbarua101@gmail.com>
2022-04-17 09:30:46 +0600

    blog section added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h1>This is a blog</h1>
+        <div className="container w-50 mt-5">
+            <h2>Why are you using firebase?</h2>
+            <p>Ans: Google firebase is a platform for developing and building dynamic web application and mobile apps. Firebase can helps to connect authentication system for different social media Like Facebook, Twitter, Github, Apple and others. It has lot's of features like Realtime database, Cloud functions, Analytics, Crashlytics, Cloud Firestore. Firebase is Easy to use. We can use backend database. </p>
+            <h2 className="mt-5">What other options do you have to implement authentication?</h2>
+            <ul>
+                <li>Watch Guard Auth point</li>
+                <li>The Octa Identity Cloud</li>
+                <li>Cisco Secure Access</li>
+                <li>Entrust Identity Enterprise</li>
+                <li>Manage Engine AD self service</li>
+                <li>Typing DNA verify</li>
+            </ul>
         </div>
     );
 };
