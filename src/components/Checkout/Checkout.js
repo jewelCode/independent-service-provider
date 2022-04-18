caea565c460192e1e70e335caab3c5bd2233f1ff
jewelCode <jewelbarua101@gmail.com>
2022-04-18 21:42:28 +0600

    check out page completed

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 
 const CheckOut = () => {
    
     return (
-        <div>
-            <h3>Checkout</h3>
-            
+        <div className="container w-50 mt-5">
+            <h3 className="text-center text-danger">Checkout Details</h3>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Billing Address</Form.Label>
+                    <Form.Control type="text" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control type="text" placeholder="Full Name" />
+                </Form.Group>
+                <h3 className="text-center text-danger">Payment Information</h3>
+                <Form.Group className="mb-3">
+                    <Form.Label>Credit Card Number</Form.Label>
+                    <Form.Control type="number" placeholder="Enter Your Credit Card Number" />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Expire Date</Form.Label>
+                    <Form.Control type="number" placeholder="Expire Date" />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>CCV</Form.Label>
+                    <Form.Control type="number" placeholder="Expire Date" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="warning" type="submit">
+                    Checkout
+                </Button>
+                <br /><br />
+                <Button variant="warning" type="submit">
+                    Back to Homepage
+                </Button>
+                </Form>
         </div>
     );
 };
